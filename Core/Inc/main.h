76c1b7aec9794b9df9cbe7ce41ee6b97d1609566
Khoabk <khoa.nguyen.bk_2019@hcmut.edu.vn>
2021-09-22 22:25:04 +0700

    adding files

---
+++ Core/Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define _1_Pin GPIO_PIN_4
+#define _1_GPIO_Port GPIOA
+#define _2_Pin GPIO_PIN_5
+#define _2_GPIO_Port GPIOA
+#define _3_Pin GPIO_PIN_6
+#define _3_GPIO_Port GPIOA
+#define _4_Pin GPIO_PIN_7
+#define _4_GPIO_Port GPIOA
+#define _5_Pin GPIO_PIN_8
+#define _5_GPIO_Port GPIOA
+#define _6_Pin GPIO_PIN_9
+#define _6_GPIO_Port GPIOA
+#define _7_Pin GPIO_PIN_10
+#define _7_GPIO_Port GPIOA
+#define _8_Pin GPIO_PIN_11
+#define _8_GPIO_Port GPIOA
+#define _9_Pin GPIO_PIN_12
+#define _9_GPIO_Port GPIOA
+#define _10_Pin GPIO_PIN_13
+#define _10_GPIO_Port GPIOA
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Core/Src/main.c
@@ -0,0 +1,315 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+
+
+
+int one_hot( int num)
+{
+
+
+
+	/*int count=0;
+
+		for (int i=5; i<65; i+=5)
+		{
+			if(num<i)
+			{
+				break;
+			}
+
+			count++;
+		}*/
+
+
+	return  num%5;  //count;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+
+
+
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+
+
+
+
+  int second=0;
+  int second_pos=0;
+
+
+  int minute=0;
+  int minute_pos=0;
+
+  int hour=0;
+
+
+
+
+  int led_pos[12][2]={{6,9},{7,9},{8,10},{8,11},
+  					{8,12},{7,13},{6,13},{5,13},
+  					{4,12},{4,11},{4,10},{5,9}};
+
+
+  int offset=990;
+
+  while (1)
+  {
+	  //the one_hot function returns the position of the led of the corresponding specific time
+	  second_pos=one_hot(second);
+
+	  minute_pos=one_hot(minute);
+
+	  int count=0;
+
+
+	  //This while loop is to perform led matrix sweeping technique in 1 seccond
+	  while(count<1000-offset)
+	  {
+
+		  int get= count%5;
+		  uint32_t get2=0;
+		  uint32_t get3=0;
+		  GPIOA->ODR |= (31<<9);
+		  GPIOA->ODR &= ~(31<<4);
+
+
+			  get2=(get==led_pos[second_pos][0]-4)?1<<led_pos[second_pos][0]:get2;
+			  get3=(get==led_pos[second_pos][0]-4)?1<<led_pos[second_pos][1]:get3;
+
+			  get2=(get==led_pos[minute_pos][0]-4)?1<<led_pos[minute_pos][0]|get2:get2;
+			  get3=(get==led_pos[minute_pos][0]-4)?1<<led_pos[minute_pos][1]|get3:get3;
+
+			  get2=(get==led_pos[hour][0]-4)?1<<led_pos[hour][0]|get2:get2;
+			  get3=(get==led_pos[hour][0]-4)?1<<led_pos[hour][1]|get3:get3;
+
+
+			  if(get2!=0)
+			  {
+				  GPIOA->ODR |= get2;
+				  GPIOA->ODR &= ~get3;
+			  }
+
+		 HAL_Delay(1);
+
+		 count++;
+	  }
+
+
+
+		  second++;
+
+
+		  if(second==60){second=0;   minute++;  }
+
+		  if(minute==60){ minute=0;  hour++;   if(hour==12)hour=0; }
+
+
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, _1_Pin|_2_Pin|_3_Pin|_4_Pin
+                          |_5_Pin|_6_Pin|_7_Pin|_8_Pin
+                          |_9_Pin|_10_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : _1_Pin _2_Pin _3_Pin _4_Pin
+                           _5_Pin _6_Pin _7_Pin _8_Pin
+                           _9_Pin _10_Pin */
+  GPIO_InitStruct.Pin = _1_Pin|_2_Pin|_3_Pin|_4_Pin
+                          |_5_Pin|_6_Pin|_7_Pin|_8_Pin
+                          |_9_Pin|_10_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
